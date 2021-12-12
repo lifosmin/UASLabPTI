6f6cf207d8476ffdbd351a37afbdfcbabd0034af
lifosmin <65651883+lifosmin@users.noreply.github.com>
2021-12-12 22:20:25 +0700

    footer sikit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import "../styles/Footer.scss";
+import {
+  Flex,
+  Link,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink
+} from "@chakra-ui/react";
 
 const Footer = () => {
   return (
     <footer>
-      <h1>ini footer</h1>
+      <Flex className="footer">
+        <Flex w="20%" className="footer__logo">
+          <h1>Berita PTI</h1>
+          <div className="footer__lv"></div>
+        </Flex>
+        <Flex w="50%" className="footer__navigation">
+          <Flex>
+            <Link p="10px">News</Link>
+            <Link p="10px">Blogs</Link>
+            <Link p="10px">Developers</Link>
+          </Flex>
+          <Flex className="footer__copyright">
+            Copyright@2021 Kelompok 1
+          </Flex>
+        </Flex>
+        <Flex w="30%" className="footer__aboutus">
+          
+        </Flex>
+      </Flex>
     </footer>
   );
 };
